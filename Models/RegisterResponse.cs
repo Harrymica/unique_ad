cc3ea8cca38711829d672cfb123c053954c8e048
Harrymica <micachidubem007@gmail.com>
2025-06-13 23:31:09 +0100

    commit

--- Models/RegisterResponse.cs
+++ Models/RegisterResponse.cs
@@ -7,4 +7,11 @@ namespace uniquead_App.Models
         public string Message { get; set; }
         public Session session { get; set; }
     }
+
+    public class UserActivity
+    {
+        public Users User { get; set; }
+        public string DotClass { get; set; }
+        public int DaysAgo { get; set; }
+    }
 }
